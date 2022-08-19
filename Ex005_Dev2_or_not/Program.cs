654ce05550ad6f3bd8af89c2af898720e92c2860
Michael <cadetvire@rambler.ru>
2022-08-20 00:06:43 +0300

    Финишная версия программы

---
+++ Ex005_Dev2_or_not/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine()!;
+int a;
+
+bool result = int.TryParse(number, out a);
+if (result)
+    Console.WriteLine("Значение числа введено корректно");
+else
+    Console.WriteLine("Некорректный ввод данных");
+
+if (result)
+{
+if (a % 2 == 0)
+{
+Console.WriteLine("Введенное число четное");
+}
+else 
+    {
+    Console.WriteLine("Введенное число нечетное");
+    }                       
+}
+else
+{
+    Console.WriteLine("Некорректно введено число");
+}
