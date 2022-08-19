e09f2d7446cda8840d1fe68803eeff50c3c2aafa
Michael <cadetvire@rambler.ru>
2022-08-19 17:48:03 +0300

    Добавили .gitignore в папку work c##

---
+++ Ex001/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число 1: ");
+string
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 1: ");
+int b = int.Parse(Console.ReadLine());
+
+
+//if (name2 == null)
+   // {
+    //Console.WriteLine("Введены не корректные данные");
+   // }
+ //else Console.WriteLine ($"Введено число: {b} ");
+
+
+ //if (name1 == null)
+   // {
+   // Console.WriteLine("Введены не корректные данные");
+   // }
+//else Console.WriteLine ($"Введено число: {a} ");
+
+//Console.WriteLine("Введите число 2: ");
