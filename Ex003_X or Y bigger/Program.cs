c7ff0464827c14ff112e514a81f964f9b722a68e
Michael <cadetvire@rambler.ru>
2022-08-19 21:14:38 +0300

    Первая версия программы

--- Ex003_X or Y bigger/Program.cs
+++ Ex003_X or Y bigger/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Введите число 1: ");
-string number1 = Console.ReadLine()!;
+string number1 = Console.ReadLine();
 int a;
 
 bool result1 = int.TryParse(number1, out a);
@@ -9,7 +9,7 @@ else
     Console.WriteLine("Некорректный ввод данных");
 
 Console.WriteLine("Введите число 2: ");
-string number2 = Console.ReadLine()!;
+string number2 = Console.ReadLine();
 int b;
 
 bool result2 = int.TryParse(number2, out b);
@@ -18,11 +18,11 @@ if (result2)
 else
     Console.WriteLine("Некорректный ввод данных");
 
-if (a == b * b)
-   Console.WriteLine($"Второе число {b} является квадратом первого числа {a}");
+if (a > b)
+   Console.WriteLine($" Первое число {a} больше второго числа {b}");
 else
-    if (b == a * a)
-       Console.WriteLine($"Первое число {a} является квадратом второго числа {b}");
+    if (b > a)
+       Console.WriteLine($"Второе число {b} больше первого числа {a}");
     else
-       if (a != b * b && b != a * a)
-          Console.WriteLine("Ни одно из чисел не является квадратом другого числа");
+       if (a == b)
+          Console.WriteLine("Число 1 и число 2 одинаковые");
