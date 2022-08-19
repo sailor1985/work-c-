777808ccb76f5de82cc1b58fc22769f1c7aaf5ad
Michael <cadetvire@rambler.ru>
2022-08-19 18:28:24 +0300

    Разобрался с Null благодаря TruParse

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,21 +1,17 @@
 ﻿Console.WriteLine("Введите число 1: ");
-string
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите число 1: ");
-int b = int.Parse(Console.ReadLine());
+string number1 = Console.ReadLine();
+int a;
+bool result1 = int.TryParse(number1, out a);
+if (result1 == true)
+    Console.WriteLine($"Преобразование прошло успешно. Число: {a}");
+else
+    Console.WriteLine("Преобразование завершилось неудачно");
 
-
-//if (name2 == null)
-   // {
-    //Console.WriteLine("Введены не корректные данные");
-   // }
- //else Console.WriteLine ($"Введено число: {b} ");
-
-
- //if (name1 == null)
-   // {
-   // Console.WriteLine("Введены не корректные данные");
-   // }
-//else Console.WriteLine ($"Введено число: {a} ");
-
-//Console.WriteLine("Введите число 2: ");
+Console.WriteLine("Введите число 2: ");
+string number2 = Console.ReadLine();
+int b;
+bool result2 = int.TryParse(number2, out b);
+if (result2 == true)
+    Console.WriteLine($"Преобразование прошло успешно. Число: {b}");
+else
+    Console.WriteLine("Преобразование завершилось неудачно");
