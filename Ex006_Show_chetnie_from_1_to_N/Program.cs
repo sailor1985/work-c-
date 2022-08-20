18f7eabc45389a3c1037f7c88f2379ddc279ac38
Michael <cadetvire@rambler.ru>
2022-08-20 20:55:59 +0300

    Финишная версия программы

---
+++ Ex006_Show_chetnie_from_1_to_N/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine()!;
+int a;
+
+bool result = int.TryParse(number, out a);
+if (result)
+{
+    Console.WriteLine("Значение числа введено корректно");
+    int res = 2;
+    int count = a;
+    if (count % 2 == 1 & res < count)
+    {
+        count--;  
+        while (count % 2 == 0 & res <= count)
+        {
+            Console.WriteLine(res);
+             res = res + 2;  
+        }
+    }
+    else
+    {
+        while (count % 2 == 0 & res <= count)
+        {
+            Console.WriteLine(res);
+            res = res+2;
+        }     
+    }
+}
+else
+    {
+        Console.WriteLine("Некорректный ввод данных");
+    }
