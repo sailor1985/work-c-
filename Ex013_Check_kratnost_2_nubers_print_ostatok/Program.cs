9eaf610e83bd9d626e599c68a77415442645891e
Michael <cadetvire@rambler.ru>
2022-08-23 01:15:22 +0300

    Финишная версия программы

---
+++ Ex013_Check_kratnost_2_nubers_print_ostatok/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int b = int.Parse(Console.ReadLine());
+
+int num = a % b;
+if (num == 0)   Console.WriteLine("Второе число кратно первому");
+else Console.WriteLine($"Второе число не кратно первому. Остаток от деления {num}");
