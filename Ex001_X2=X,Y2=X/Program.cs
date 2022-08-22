d8b407c84b9998c1df1932204451fd42b52b9e1f
Michael <cadetvire@rambler.ru>
2022-08-22 22:32:23 +0300

    Итоговая версия с учетом оптимизации кода (убрали большинство цикло if и сделали if else

--- Ex001_X2=X,Y2=X/Program.cs
+++ Ex001_X2=X,Y2=X/Program.cs
@@ -4,9 +4,13 @@ int a;
 
 bool result1 = int.TryParse(number1, out a);
 if (result1)
+{
     Console.WriteLine($"Значение {a} введено корректно");
+}
 else
-    Console.WriteLine("Некорректный ввод данных");
+{
+    Console.WriteLine("Некорректный ввод числа 1");
+}
 
 Console.WriteLine("Введите число 2: ");
 string number2 = Console.ReadLine()!;
@@ -14,15 +18,26 @@ int b;
 
 bool result2 = int.TryParse(number2, out b);
 if (result2)
+{
     Console.WriteLine($"Значение {b} введено корректно");
+}
 else
-    Console.WriteLine("Некорректный ввод данных");
-
-if (a == b * b)
-   Console.WriteLine($"Второе число {b} является квадратом первого числа {a}");
-else
-    if (b == a * a)
-       Console.WriteLine($"Первое число {a} является квадратом второго числа {b}");
-    else
-       if (a != b * b && b != a * a)
-          Console.WriteLine("Ни одно из чисел не является квадратом другого числа");
+{
+    Console.WriteLine("Некорректный ввод числа 2");
+}
+if (result1 == false && result2 == false )
+{
+  Console.WriteLine("Некорректный ввод данных обоих чисел"); 
+}
+if (b == a * a)
+{
+    Console.WriteLine($"Первое число {a} является квадратом второго числа {b}");
+}
+else if (a == b * b)
+{
+    Console.WriteLine($"Второе число {b} является квадратом первого числа {a}");
+}
+else if (b != a * a && a != b * b)
+{
+    Console.WriteLine("Ни одно из чисел не является квадратом другого числа");
+}
