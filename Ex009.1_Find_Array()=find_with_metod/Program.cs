f6e5b2f560584d185233abe2f53f698db3cb3449
Michael <cadetvire@rambler.ru>
2022-08-22 20:18:43 +0300

    Финишная версия программы

---
+++ Ex009.1_Find_Array()=find_with_metod/Program.cs
@@ -0,0 +1,44 @@
+﻿var test = new Random();
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = test.Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+//int find = 31;
+int Indexof(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        position =index;
+        break;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+int pos = Indexof(array, 4);
