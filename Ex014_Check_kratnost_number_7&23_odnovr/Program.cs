b6222140e39f3ffeccd0cbc406a104746cf4dc86
Michael <cadetvire@rambler.ru>
2022-08-23 01:19:43 +0300

    Финишная версия программы

---
+++ Ex014_Check_kratnost_number_7&23_odnovr/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 7 == 0 & a % 23 == 0) Console.WriteLine($"Введеное число {a} одновременно кратно и 7 и 23"); 
+else Console.WriteLine($"Введеное число {a} не кратно ни 7, ни 23");
