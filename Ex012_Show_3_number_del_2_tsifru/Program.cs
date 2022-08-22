a8bbf41690adc36c619e6950322d6d4d9a89a8cf
Michael <cadetvire@rambler.ru>
2022-08-23 00:59:40 +0300

    Финишная версия программмы

---
+++ Ex012_Show_3_number_del_2_tsifru/Program.cs
@@ -0,0 +1,6 @@
+﻿int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a2 = num % 10;
+int res= a1 *10 + a2;
+
+Console.WriteLine($"Трехзначное число равно {num}. Без второй цифры будет число  {res}");
