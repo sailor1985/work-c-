a2efe2157c65c4d4f69374ead07109a7e0e56d6d
Michael <cadetvire@rambler.ru>
2022-08-24 00:50:11 +0300

    Финишная версия программы

---
+++ Ex016_Take_3_number_and_show__3_tsifru_or_say_about_her_absence/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string s = Console.ReadLine()!;
+int a = int.Parse(s);
+if (a < 100)
+{
+    Console.WriteLine($"У введенного числа {a} третьей цифры нет");
+}
+else if (a >= 100)
+{
+    Console.WriteLine($"У введенного числа {a} третьей цифрой является {s[2]} ");  
+}
