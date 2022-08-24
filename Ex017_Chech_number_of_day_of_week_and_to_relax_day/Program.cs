e4e6860bf4a55a7810b4e86a8d75ea031102476c
Michael <cadetvire@rambler.ru>
2022-08-24 12:51:16 +0300

    Финишная версия программы

---
+++ Ex017_Chech_number_of_day_of_week_and_to_relax_day/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели:");
+string day_number = Console.ReadLine()!;
+switch (day_number)
+{
+    case "1":
+        {
+            Console.WriteLine("День не является выходным (Понедельник)");
+            break;
+        }
+    case "2":
+        {
+            Console.WriteLine("День не является выходным (Вторник)");
+            break;
+        }
+    case "3":
+        {
+            Console.WriteLine("День не является выходным (Среда)");
+            break;
+        }
+    case "4":
+        {
+            Console.WriteLine("День не является выходным (Четверг)");
+            break;
+        }
+    case "5":
+        {
+            Console.WriteLine("День не является выходным (Пятница)");
+            break;
+        }
+    case "6":
+        {
+            Console.WriteLine("Ура! День ЯВЛЯЕТСЯ выходным (Суббота)");
+            break;
+        }
+    case "7":
+        {
+            Console.WriteLine("Ура! День ЯВЛЯЕТСЯ выходным (Воскресенье)");
+            break;
+        }
+    default:
+        {
+            Console.WriteLine("Введенная цифра не соответствует дню недели");
+            break;
+        }
+}
