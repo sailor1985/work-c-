2aa7bc16c81ba623663e46c1f16cda82cba7e101
Michael <cadetvire@rambler.ru>
2022-08-26 00:09:07 +0300

    Финишная версия программы

---
+++ Ex018_input_nomer_chetvert_output_diap_koord/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите четверть");
+int x = Convert.ToInt32(Console.ReadLine());
+
+switch(x)
+{
+    case 1:
+    {
+        Console.WriteLine("x > 0; y > 0");
+        break;
+    }
+    case 2:
+    {
+        Console.WriteLine("x < 0; y > 0");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("x < 0; y < 0");
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("x > 0; y < 0");
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Введенная цифра не соответствует номеру четверти");
+        break;
+    }
+}
