39c26c218f368d382312803f4c38ae32f535a772
Michael <cadetvire@rambler.ru>
2022-08-26 01:30:14 +0300

    Финишная версия программы

---
+++ Ex023_input_N_output_kubi_numbers_1-N/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+string result = "";
+for (int i = 1; i <= number; i++)
+{
+    double res = Math.Pow(i, 3);
+    result = result + res + " | ";
+}
+Console.WriteLine($"Таблица кубов чисел от 1 до {number} следующая:");
+Console.WriteLine (result);
