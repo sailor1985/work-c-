39427df79d51ae2272fdaab270c186515a8eeeb9
Michael <cadetvire@rambler.ru>
2022-08-26 01:19:28 +0300

    Дополнил текст про 3D

--- Ex022_input_koord_2_points_3D_output_rasst_mezdu_nimi/Program.cs
+++ Ex022_input_koord_2_points_3D_output_rasst_mezdu_nimi/Program.cs
@@ -17,4 +17,4 @@ Console.WriteLine("Введите координаты Z2: ");
 double z2 = Double.Parse(Console.ReadLine()!);
 
 double result = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2)+Math.Pow(z2-z1, 2));
-Console.WriteLine($"Расстояние между двумя точками с координатами ({x1},{y1},{z1}) и ({x2},{y2},{z2}) равно {result}");
+Console.WriteLine($"Расстояние между двумя точками с координатами ({x1},{y1},{z1}) и ({x2},{y2},{z2}) равно в 3D пространстве {result}");
