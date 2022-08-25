7b458b50a22596d31dc7818112e1f3946c584965
Michael <cadetvire@rambler.ru>
2022-08-26 01:08:06 +0300

    Финишная версия программы

---
+++ Ex021_input_number_check_for_polindrom/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine()!);
+int tsifra_1_of_number = number / 10000;
+int tsifra_5_of_number = number % 10;
+
+int tsifra_2_of_number = (number / 1000) % 10;
+int tsifra_4_of_number = (number % 100) / 10;
+
+if (tsifra_1_of_number == tsifra_5_of_number && tsifra_2_of_number == tsifra_4_of_number)
+{
+    Console.WriteLine($"Введенное число {number} является полиномом");
+}
+else Console.WriteLine($"Введенное число {number} не является полиномом");
