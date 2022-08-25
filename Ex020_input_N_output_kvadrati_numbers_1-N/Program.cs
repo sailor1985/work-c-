bfb820f3a529ca415bbadd1fdec6f4ad65c96639
Michael <cadetvire@rambler.ru>
2022-08-25 23:47:44 +0300

    Финишная версия программы

---
+++ Ex020_input_N_output_kvadrati_numbers_1-N/Program.cs
@@ -0,0 +1,8 @@
+﻿int max = int.Parse(Console.ReadLine()!);
+string result = "";
+for (int i = 1; i <= max; i++)
+{
+    double res = Math.Pow(i, 2);
+    result = result + res + " | ";
+}
+Console.WriteLine(result);
