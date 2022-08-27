b7b39d4cfb01a53f525f7890b332f25bd8f503d3
Michael <cadetvire@rambler.ru>
2022-08-27 17:00:28 +0300

    Добавили вторую замену текста: маленькие буквы "к" заменили на "К"

--- Ex026_change_symvols_of_text/Program.cs
+++ Ex026_change_symvols_of_text/Program.cs
@@ -23,4 +23,8 @@ string Replace(string text, char oldValue, char newValue)
 }
 
 string newText = Replace(text, ' ', '/');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
 Console.WriteLine(newText);
