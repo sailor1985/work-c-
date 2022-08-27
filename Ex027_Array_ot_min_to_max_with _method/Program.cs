9f96fec63bf213c77fbad62534cb18737302c3b0
Michael <cadetvire@rambler.ru>
2022-08-27 17:17:30 +0300

    Написали код для печати массива через метод

---
+++ Ex027_Array_ot_min_to_max_with _method/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
