2aa6525c9fbeb7c15ddb4ff80830ca9acda13e6f
Michael <cadetvire@rambler.ru>
2022-08-27 17:35:50 +0300

    Добавил пробел в выводе массива

--- Ex027_Array_ot_min_to_max_with _method/Program.cs
+++ Ex027_Array_ot_min_to_max_with _method/Program.cs
@@ -4,7 +4,7 @@ void PrintArray(int[] array)
     int count = array.Length;
     for (int i = 0; i < count; i++)
     {
-        Console.Write($"{array[i]}");
+        Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
 }
