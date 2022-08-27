0d0b5675aeac5c9f28e99d5a896b411866c03afb
Michael <cadetvire@rambler.ru>
2022-08-27 17:32:08 +0300

    Упорядочили элементы массива от min  к max и вывели его

--- Ex027_Array_ot_min_to_max_with _method/Program.cs
+++ Ex027_Array_ot_min_to_max_with _method/Program.cs
@@ -8,4 +8,24 @@ void PrintArray(int[] array)
     }
     Console.WriteLine();
 }
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
 PrintArray(arr);
