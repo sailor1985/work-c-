80fbab6766280cdadca8b14ecb253cfdd535c119
Michael <cadetvire@rambler.ru>
2022-08-29 21:36:51 +0300

    Финишная версия программы

---
+++ Ex030/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+int Method (int numberA)
+{
+int num = 1;
+int fact = 1;
+while (num <= numberA)
+{
+    fact = fact *num;
+    num = num +1;
+}
+    return fact;
+}
+
+Console.WriteLine(Method(number));
