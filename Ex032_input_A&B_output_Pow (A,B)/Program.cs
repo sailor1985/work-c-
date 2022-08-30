1a96dad4b52861366507516af0bc5b9d7a66d9fa
Michael <cadetvire@rambler.ru>
2022-08-31 00:39:27 +0300

    Финишная версия программы

---
+++ Ex032_input_A&B_output_Pow (A,B)/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+//и возводит число A в натуральную степень B.
+Console.WriteLine("Введите число A");
+int numberA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число B");
+int numberB = int.Parse(Console.ReadLine()!);
+
+double Method(double A, double B)
+{
+    double result = 0;
+    if (A > 0 && B > 0)
+    {
+        result = Math.Pow(A, B);
+    }
+    return result;
+}
+
+Console.WriteLine(Method(numberA, numberB));
