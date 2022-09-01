53a15f11df9e67a6305c8d09a99e7aee80261b77
Michael <cadetvire@rambler.ru>
2022-09-02 00:05:51 +0300

    Финишная версия программы

---
+++ Ex036_input_Array[8]_output_PrintArray/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+void fillArray(int[] fill)
+{
+    var test = new Random();
+    for (int i = 0; i < fill.Length; i++)
+    {
+        fill[i] = test.Next(1,100);
+    }
+}
+
+void printArray(int[] print)
+{
+    Console.WriteLine("Вывод массива: ");
+    for (int i = 0; i < print.Length; i++)
+    {
+        Console.Write(print[i] + " ");
+    }
+}
+
+int[] array = new int[8];
+
+fillArray(array);
+printArray(array);
