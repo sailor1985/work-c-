b6beebdd7b989c23b1ebad29b572b03ffddd007e
Michael <cadetvire@rambler.ru>
2022-09-01 23:37:25 +0300

    Финишная версия программы

---
+++ Ex035_find_number_of_array_and_say_see_him/Program.cs
@@ -0,0 +1,38 @@
+﻿void fillArray(int[] fill)
+{
+    var test = new Random();
+    for (int i = 0; i < fill.Length; i++)
+    {
+        fill[i] = test.Next(1,100);
+    }
+}
+
+void printArray(int[] print)
+{
+    Console.WriteLine("Вывод массива: ");
+    for (int i = 0; i < print.Length; i++)
+    {
+        Console.Write(print[i] + " ");
+    }
+}
+
+string search(int[] array_num, int num)
+{
+    for (int i = 0; i < array_num.Length; i++)
+    {
+        if (array_num[i] == num)
+        {
+            return "Заданное число в массиве ЕСТЬ";
+        }
+    }
+    return "Заданного числа в массиве НЕТ";
+}
+
+Console.WriteLine("Введите число от 1 до 100, которое хотите найти в массиве: ");
+int number = int.Parse(Console.ReadLine()!);
+int[] array = new int[12];
+
+fillArray(array);
+printArray(array);
+Console.WriteLine();
+Console.WriteLine(search(array, number));
