1f050bce4d38f82f9d92f6ba1e93adf6f549608d
Michael <cadetvire@rambler.ru>
2022-09-02 00:00:28 +0300

    Финишная версия программы

---
+++ Ex033_input_N_output_sum_digits_of_N/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int numberOfDigits(int numberA)
+{
+    int sum = 0;
+    while (numberA != 0)
+    {
+        sum += numberA % 10;
+        numberA = numberA / 10;
+    }
+    return sum;
+}
+
+int sumNumber = numberOfDigits(number);
+Console.WriteLine($"Сумма цифр в числе {number} равна {sumNumber}");
