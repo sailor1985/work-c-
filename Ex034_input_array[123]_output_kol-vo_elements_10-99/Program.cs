140f40e9d5109da3998416298059e74d9a573bd3
Michael <cadetvire@rambler.ru>
2022-09-01 23:16:50 +0300

    Отслеживаем все файлы проекта

---
+++ Ex034_input_array[123]_output_kol-vo_elements_10-99/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задайте одномерный массив из 123 случайных чисел.
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+var test = new Random();
+void fillArray(int[] fill)
+{
+    for (int i = 0; i < fill.Length; i++)
+    {
+        fill[i] = test.Next(1,200);
+    }
+}
+
+void printArray(int[] print)
+{
+    Console.WriteLine("Вывод массива: ");
+    for (int i = 0; i < print.Length; i++)
+    {
+        Console.Write(print[i] + " ");
+    }
+}
+
+int Col(int[] collection)
+{
+    int result = 0;
+    for (int i = 0; i < collection.Length; i++)
+    {
+        if (collection[i] > 10 && collection[i] < 99)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+int[] array = new int[123];
+
+fillArray(array);
+printArray(array);
+Col(array);
+int res = Col(array);
+Console.WriteLine();
+Console.Write($"Количество элементов массива от 10 до 99 равно {res}");
