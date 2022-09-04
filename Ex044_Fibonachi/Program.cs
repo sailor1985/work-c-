1e608875b031afaec49ea7ad6d96b8401e418c04
Michael <cadetvire@rambler.ru>
2022-09-04 20:19:47 +0300

    Финишная версия программф

--- Ex044_Fibonachi/Program.cs
+++ Ex044_Fibonachi/Program.cs
@@ -1,9 +1,9 @@
-﻿int Fibonacci(int n)
+﻿double Fibonacci(int n)
 {
     if (n == 1 || n == 2) return 1;
     else return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
-for (int i = 1; i < 10; i++)
+for (int i = 1; i < 50; i++)
 {
-    Console.WriteLine(Fibonacci(i));
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
 }
