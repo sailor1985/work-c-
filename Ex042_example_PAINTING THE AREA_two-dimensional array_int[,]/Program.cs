040121210a55559e7f50ebd489759060f38632d7
Michael <cadetvire@rambler.ru>
2022-09-04 19:43:27 +0300

    Закрасили "Класс" "+" полностью

--- Ex042_example_PAINTING THE AREA_two-dimensional array_int[,]/Program.cs
+++ Ex042_example_PAINTING THE AREA_two-dimensional array_int[,]/Program.cs
@@ -38,4 +38,17 @@ void PrintImage(int[,] image)
     }
 }
 
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+PrintImage(pic);
+FillImage(13, 13);
 PrintImage(pic);
