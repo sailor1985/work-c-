c7a4dc401823ad23716728ec7f0778dc2db3877a
Michael <cadetvire@rambler.ru>
2022-09-05 23:24:42 +0300

    Финишная версия программы

---
+++ Ex047_input_M_number of numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+Console.WriteLine("Введите 5 чисел через пробел: ");
+string[] st = Console.ReadLine()!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+int numberOfNumbersGreaterThanZeros(int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+int[] array = {int.Parse(st[0]), int.Parse(st[1]), int.Parse(st[2]),
+              int.Parse(st[3]), int.Parse(st[4])};
+
+numberOfNumbersGreaterThanZeros(array);
+int res = numberOfNumbersGreaterThanZeros(array);
+Console.WriteLine();
+Console.Write($"Количество чисел больше 0 равно {res}");
