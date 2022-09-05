adc6806b00c13714b55ac065e2147f39a4149196
Michael <cadetvire@rambler.ru>
2022-09-05 22:24:13 +0300

    Финишная версия программы

---
+++ Ex045/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+string res1 = Convert.ToString(number, 2);
+string res2 = DecToNum(number, 2);
+
+Console.WriteLine($"{number}->{res1}");
+
+// Универсальный математический для перевода из 10 в любую
+string DecToNum(int decNumber, int otherSystem)
+{
+  string res = "";
+  string nums = "0123456789ABCDEF";
+  while (decNumber > 0)
+  {
+    int ost = decNumber / otherSystem;
+    res = nums[decNumber - otherSystem * ost] + res;
+    decNumber /= otherSystem;
+  }
+  return res;
+}
---
+++ Ex046_input_3_numbers_check_checking the triangle/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход три числа и проверяет, 
+//может ли существовать треугольник с сторонами такой длины.
+Console.WriteLine("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 3: ");
+int number3 = int.Parse(Console.ReadLine());
+
+void Treug(int a, int b, int c)
+{
+    if (a + b > c || b + c > a || c + a > b)
+        Console.WriteLine($"Треугольник со сторонами такой длины может существовать");
+    else Console.WriteLine($"Треугольник со сторонами такой длины может существовать");
+}
+
+Treug(number1, number2, number3);
