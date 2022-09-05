13fbd1edb5fc1dcd42eb9856841bc1bae2574249
Michael <cadetvire@rambler.ru>
2022-09-05 22:29:47 +0300

    Финиш

--- Ex046_input_3_numbers_check_checking the triangle/Program.cs
+++ Ex046_input_3_numbers_check_checking the triangle/Program.cs
@@ -1,13 +1,13 @@
 ﻿//Напишите программу, которая принимает на вход три числа и проверяет, 
 //может ли существовать треугольник с сторонами такой длины.
 Console.WriteLine("Введите число 1: ");
-int number1 = int.Parse(Console.ReadLine());
+int number1 = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введите число 2: ");
-int number2 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Введите число 3: ");
-int number3 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine()!);
 
 void Treug(int a, int b, int c)
 {
