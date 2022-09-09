f8f1889a4bff759062019a9f2fc363f5a7a7825b
Michael <cadetvire@rambler.ru>
2022-09-09 21:35:12 +0300

    Финишная версия программы

---
+++ Ex050_Array[m,n], Array[i,j]=i+j/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задайте двумерный массив размера m на n,
+//каждый элемент в массиве находится по формуле: A(m,n) = m+n.
+//Выведите полученный массив на экран.
+
+int[,] GetArray(int m, int n)
+{
+    int[,] res = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            res[i, j] = i + j;
+        }
+    }
+    return res;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns);
+
+PrintArray(array);
