f231499ccaed05e51940a95e863e6ececbdd4a7e
Michael <cadetvire@rambler.ru>
2022-09-09 22:21:05 +0300

    Финишная версия программы

---
+++ Ex051_Array[i+2,j+2]=Array[i+2,j+2]^2/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные,
+//и замените эти элементы на их квадраты.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] res = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            res[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return res;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j] + "\t"}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void EvenIndex(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < array.GetLength(1); j += 2)
+        {
+            array[i, j] *= array[i, j];
+        }
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+
+PrintArray(array);
+
+EvenIndex(array);
+
+PrintArray(array);
