941f31af76dc1b16cf52400dfba8cc5ad464e758
Michael <cadetvire@rambler.ru>
2022-09-15 22:51:21 +0300

    Финишная версия программы

--- Ex057_InArray2K_find_SumElements_every_row&show_max_sum_/Program.cs
+++ Ex057_InArray2K_find_SumElements_every_row&show_max_sum_/Program.cs
@@ -24,57 +24,28 @@ void PrintArray2K(int[,] inArray)
     }
 }
 
-void PrintArray1K(int[] print)
-{
-    Console.WriteLine("Вывод массива: ");
-    for (int i = 0; i < print.Length; i++)
-    {
-        Console.Write(print[i] + " ");
-    }
-    Console.WriteLine();
-}
-
-int FindMin1K(int[] array)
-{
-    int min = 0;
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        int minPosition = i;
-        min = minPosition;
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if (array[j] < array[minPosition]) minPosition = j;
-        }
-        int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
-    }
-    Console.WriteLine($"В {min} строке самая минимальная сумма всех ее элементов");
-    return min;
-}
-
-int[] SortSumOfRows(int[,] array)
+void MinSumRow(int[,] array)
 {
     int rows = array.GetLength(0);
     int columns = array.GetLength(1);
-    int[] sortSum = new int[rows];
+    int minSum = Int32.MaxValue;
+    int minRow = 0;
     for (int i = 0; i < rows; i++)
     {
-        int tempSum = 0;
+        int sum = 0;
         for (int j = 0; j < columns; j++)
         {
-            tempSum += array[i, j];
+            sum += array[i, j];
         }
-        Console.WriteLine($"Сумма элементов в {i} строке, равна {tempSum}");
-        sortSum[i] = tempSum;
+        Console.WriteLine($"Сумма элементов в {i + 1} строке, равна {sum}");
+        if (sum < minSum)
+        {
+            minSum = sum;
+            minRow = i;
+        }
     }
-
     Console.WriteLine();
-    PrintArray1K(sortSum);
-    FindMin1K(sortSum);
-    //Console.WriteLine($"В {minIndex} строке самая минимальная сумма всех ее элементов");
-    PrintArray1K(sortSum);
-    return sortSum;
+    Console.WriteLine($"В {minRow + 1} строке самая минимальная сумма всех ее элементов");
 }
 
 void Result()
@@ -82,11 +53,10 @@ void Result()
     int rows = new Random().Next(4, 8);
     int columns = new Random().Next(4, 8);
     int[,] array = new int[rows, columns];
-    int[] sortSum = new int[rows];
     GetArray2K(array, 0, 10);
     PrintArray2K(array);
     Console.WriteLine();
-    SortSumOfRows(array);
+    MinSumRow(array);
 }
 
 Result();
