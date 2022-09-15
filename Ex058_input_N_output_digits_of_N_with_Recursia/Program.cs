e8820185b9045d090d094c325eb3abecf58a6b3b
Michael <cadetvire@rambler.ru>
2022-09-15 22:34:05 +0300

    Финишная версия

---
+++ Ex058_input_N_output_digits_of_N_with_Recursia/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 63: Задайте значение N.
+//Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(PrintNumbers(1, number));
