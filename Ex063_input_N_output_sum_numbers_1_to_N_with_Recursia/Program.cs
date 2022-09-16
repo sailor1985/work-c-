da3f3c15a45c333fefa9266674ca11b80d491f78
Michael <cadetvire@rambler.ru>
2022-09-16 23:42:36 +0300

    Финишная версия программы

---
+++ Ex063_input_N_output_sum_numbers_1_to_N_with_Recursia/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 63: Задайте значение N.
+//Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+int SumNumbers(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    return (number + SumNumbers(number - 1));
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма чисел от 1 до " + number + " равна " + SumNumbers(number));
