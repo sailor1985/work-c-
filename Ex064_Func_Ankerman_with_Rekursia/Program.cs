a4865723843b16b76b1d429b3b96cb82665f8339
Michael <cadetvire@rambler.ru>
2022-09-17 00:00:03 +0300

    Финишная версия программы

---
+++ Ex064_Func_Ankerman_with_Rekursia/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//Даны два неотрицательных числа m и n.
+//n = 2, m = 3 -> A(m,n) = 29
+
+int recursion(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    } // Шаг рекурсии / рекурсивное условие
+    else if (n == 0 && m > 0)
+    {
+        return recursion(m - 1, 1);
+    } // Шаг рекурсии / рекурсивное условие
+    else
+    {
+        return recursion(m - 1, recursion(m, n - 1));
+    }
+}
+
+void Method()
+{
+    Console.WriteLine("Введите число n:");
+    int n = int.Parse(Console.ReadLine()!);
+    
+    Console.WriteLine("Введите число m:");
+    int m = int.Parse(Console.ReadLine()!);
+ 
+    Console.Write($"A ({n},{m}) = ");
+    Console.WriteLine(recursion(m, n)); // вызов рекурсивной функции
+}
+
+Method();
