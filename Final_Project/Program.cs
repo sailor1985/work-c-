13e5b1e0cc10765d2756eff3283c2cccf32cd505
Michael <cadetvire@rambler.ru>
2022-09-29 23:01:28 +0300

    Создан строковый массив с вводом элементов с клавиатуры и выведен на печать

---
+++ Final_Project/Program.cs
@@ -0,0 +1,40 @@
+﻿// Программа преобразовывает массив строк в массив строк длиной <=3
+Console.WriteLine("Введите количество элементов в массиве:");
+int length = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+void fillArray(string[] fill)
+{
+    Console.WriteLine($"Введите {fill.Length} элемента(ов) массива");
+    Console.WriteLine($"По окончанию ввода каждого элемента нажмите [Enter]");
+    for (int i = 0; i < fill.Length; i++)
+    {
+        fill[i] = Console.ReadLine()!;
+    }
+    Console.WriteLine();
+}
+
+void printArray(string[] print)
+{
+    Console.WriteLine("Введен следующий строковый массив: ");
+    for (int i = 0; i < print.Length; i++)
+    {
+        Console.Write(print[i] + "\t");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+string[] Elements1 = new string[length];
+
+fillArray(Elements1);
+printArray(Elements1);
+
+Console.Write("Нажмите [Enter] для выхода...");
+Console.ReadLine();
+
+//string[] Elements2 = new string[3];
+//for (int i = 0; i < 3; i++)
+//{
+// Elements1[i] = Console.ReadLine()!;
+//}
